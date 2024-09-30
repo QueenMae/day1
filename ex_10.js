e8e7bfbd691a7e072361a7592f909032bfaddf0f
QueenMae <maena.brice@epitech.digital>
2024-09-30 17:25:12 +0200

    ex_10.js

---
+++ ex_10.js
@@ -0,0 +1,11 @@
+if (breadCount >= 2 && hamCount >= 1 && tunaCount >=1) {
+displayThisText("I can make a ham sandwich");
+}
+else if (breadCount >= 2 && tunaCount >= 1 &&  saladCount >=2 ) {
+displayThisText("I can make a tuna sandwich");
+}
+else if { 
+if (breadCount >= 2 && saladCount >= 1 && tomatoCount >=2) {
+displayThisText("I can make a vegetarian sandwich");
+}
+displayIngredients
