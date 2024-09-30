e07790af0afb657ea486e230aee9aa52a45fdb7f
QueenMae <maena.brice@epitech.digital>
2024-09-30 12:29:35 +0200

    ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,10 @@
+let bool = "true<br/>"
+document.write(bool);
+let float = "forty two<br/>"
+document.write(float);
+let string = "42<br/>"
+document.write(string);
+let integer = ""
+docuement.write(integer);
+let this_is_null = "42.42"
+document.write(this_is_null);
