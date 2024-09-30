5a734063f0eb3c3d168a5b1963a53cb4b4ff90c9
QueenMae <maena.brice@epitech.digital>
2024-09-30 14:23:38 +0200

    ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+displayThisText(typeof myhouse);
+displayThisText (typeof true);
+displayThisText (typeof 3);
+displayThisText (typeof 43n);
+displayThisText (typeof null);
+displayThisText (typeof $)
+displayThisText (typeof Symbol())
